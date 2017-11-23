22ed262b92e896ec83ced04b53fd017bfd287f2d
Ilya <int3rsys@live.com>
2017-11-23 14:28:07 +0200

    Merge branch 'master' of C:\Users\Ilya\Desktop\MATAM\hw2\Assinment2 with conflicts.

--- Course.c
+++ Course.c
@@ -4,33 +4,34 @@
 
 #include "Course.h"
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 
-static char *duplicateString(const char *str) {
+static char* duplicateString(const char* str)
+{
     int new_string_size = strlen(str);
-    char *duplicated_string = malloc(new_string_size * sizeof(char) + 1);
-    strcpy(duplicated_string, str);
+    char* duplicated_string = malloc(new_string_size*sizeof(char) + 1);
+    strcpy(duplicated_string,str);
     if (duplicated_string == NULL) return NULL;
     return duplicated_string;
 }
 
-static int compareCoursesById(Course course1, Course course2) {
+static int compareCoursesById(Course course1, Course course2)
+{
     int compare_result = strcmp(course1->id, course2->id);
     return compare_result;
 }
 
-CourseResult
-createCourse(char *id, char *name, double credits, Course *course) {
+CourseResult createCourse(char *id, char *name, double credits, Course *course)
+{
     assert(name != NULL && id != NULL && course != NULL);
-    if (credits < 0) return COURSE_ILLEGAL_PARAMETER;
+    if(credits < 0) return COURSE_ILLEGAL_PARAMETER;
     Course new_course;
     new_course = malloc(sizeof(*new_course));
     DynamicArray pre_courses;
     pre_courses = createDynamicArray();
-    if (pre_courses == NULL) return COURSE_MEMORY_ERROR;
+    if(pre_courses == NULL) return COURSE_MEMORY_ERROR;
     new_course->preCourses = pre_courses;
     new_course->id = duplicateString(id);
     new_course->name = duplicateString(name);
@@ -39,24 +40,32 @@ createCourse(char *id, char *name, double credits, Course *course) {
     return COURSE_OK;
 }
 
-int coursesEqualId(Course course1, Course course2) {
+int coursesEqualId(Course course1, Course course2)
+{
     assert(course1 != NULL && course2 != NULL);
     int compare_result;
     compare_result = compareCoursesById(course1, course2);
-    if (compare_result == 0) {
+    if (compare_result == 0)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-int courseLessThan(Course course1, Course course2) {
+int courseLessThan(Course course1, Course course2)
+{
     assert(course1 != NULL && course2 != NULL);
     int compare_result;
-    compare_result = compareCoursesById(course1, course2);
-    if (compare_result < 0) {
+    compare_result = compareCoursesById(course1,course2);
+    if (compare_result < 0)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
@@ -65,7 +74,7 @@ CourseResult courseUpdateName(Course course1, char *new_name) {
     assert(course1 != NULL && new_name != NULL);
     char *temp_store_name = course1->name;
     char *name = duplicateString(new_name);
-    if (name == NULL) return COURSE_MEMORY_ERROR;
+    if (name == NULL)   return COURSE_MEMORY_ERROR;
     course1->name = name;
     free(temp_store_name);
     return COURSE_OK;
@@ -85,7 +94,8 @@ void displayCourse(Course course1) {
     }
 }
 
-void destroyCourse(Course course1) {
+void destroyCourse(Course course1)
+{
     free(course1->id);
     free(course1->name);
     destroyDynamicArray(course1->preCourses);
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 //
+//<<<<<<< Updated upstream
 // Created by Omer on 22/11/2017.
 //
 
@@ -6,19 +7,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
 #include "Course.h"
+
+
+//int main(){
+
+//########################## OMER ###############################
+
+
+//########################## ILIYA ###############################
+/*
+    return 0;
+=======*/
+// Created by Ilya on 11/22/2017.
+//
+
 
 int main()
 {
-    Course c1;
-    createCourse("234111", "Computer Science", 3.0, &c1);
-    Course c2;
-    createCourse("234118", "Omer", 3.0, &c2);
+    Course tst,tst2;
+    CourseResult rsl1 = createCourse("FUCK01", "whatthehell", 0.01, &tst);
+    CourseResult rsl2 = createCourse("LITASFUCKKKKKk", "yeahright", 666, &tst2);
+    if (rsl1 == COURSE_OK && rsl2 == COURSE_OK) printf("...nothing here...\n");
+    printf("%s less than %s ? %s\n", tst->id,tst2->id, courseLessThan(tst,tst2) ? "yes":"no");
+    printf("%s equal to %s ? %s", tst->id,tst2->id, coursesEqualId(tst,tst2) ? "yes":"no");
 
-    addElementStart(c1->preCourses, c2);
-
-    displayCourse(c1);
-
-    return 0;
+//>>>>>>> Stashed changes
 }
