5a0e85b6804e4e3c9d02620bed13d288a316ebc5
Omer <omer.be@campus.technion.ac.il>
2017-11-23 17:58:26 +0200

    add duplicateString function

--- CourseSystem.c
+++ CourseSystem.c
@@ -2,5 +2,15 @@
 // Created by Omer on 23/11/2017.
 //
 
+#include <stdlib.h>
+#include <string.h>
 #include "CourseSystem.h"
+
+static char *duplicateString(const char *str) {
+    int new_string_size = strlen(str);
+    char *duplicated_string = malloc(new_string_size * sizeof(char) + 1);
+    strcpy(duplicated_string, str);
+    if (duplicated_string == NULL) return NULL;
+    return duplicated_string;
+}
 
