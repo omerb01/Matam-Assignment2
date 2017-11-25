88fd1cddd1082d45c2f95464822f113f8e2aad84
Ilya <int3rsys@live.com>
2017-11-25 15:54:00 +0200

    Minor changes to Cource.c + Adding my functions to coursesystem.c

--- Course.c
+++ Course.c
@@ -6,67 +6,82 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
-CourseResult removePreCourse(Course course1, Course course2){return COURSE_OK;}
+#define CHECK_DA_MEMORY_ERROR($$function$$) \
+DAResult $$error$$ = ($$function$$) ; \
+if ($$error$$ == DA_MEMORY_ERROR) {return COURSE_MEMORY_ERROR;} \
+else if ($$error$$ == DA_ILLEGAL_INDEX) {return COURSE_ILLEGAL_PARAMETER;} \
 
-static char* duplicateString(const char* str)
-{
+// TODO: CHECK WITH OMER THE ILLEGAL INDEX, SHOULDN'T GET THIS ERROR ANYHOW ****
+
+static char *duplicateString(const char *str) {
     int new_string_size = strlen(str);
-    char* duplicated_string = malloc(new_string_size*sizeof(char) + 1);
-    strcpy(duplicated_string,str);
+    char *duplicated_string = malloc(new_string_size * sizeof(char) + 1);
     if (duplicated_string == NULL) return NULL;
+    strcpy(duplicated_string, str);
     return duplicated_string;
 }
 
-static int compareCoursesById(Course course1, Course course2)
-{
+static int compareCoursesById(Course course1, Course course2) {
     int compare_result = strcmp(course1->id, course2->id);
     return compare_result;
 }
 
-CourseResult createCourse(char *id, char *name, double credits, Course *course)
-{
+static Course getPreCourse(Course c, int index) {
+    return c->preCourses->elements[index];
+}
+
+static int isPreCourseExist(Course c1, Course c2) {
+    int index_of_c2;
+    CHECK_DA_MEMORY_ERROR(indexOfElement(c1->preCourses, c2, 0, &index_of_c2));
+    if (index_of_c2 == -1) {
+        return 0;
+    } else {
+        return 1;
+    }
+
+}
+
+CourseResult
+createCourse(char *id, char *name, double credits, Course *course) {
     assert(name != NULL && id != NULL && course != NULL);
-    if(credits < 0) return COURSE_ILLEGAL_PARAMETER;
+    if (credits < 0) return COURSE_ILLEGAL_PARAMETER;
+
     Course new_course;
     new_course = malloc(sizeof(*new_course));
     DynamicArray pre_courses;
     pre_courses = createDynamicArray();
-    if(pre_courses == NULL) return COURSE_MEMORY_ERROR;
+    if (pre_courses == NULL) return COURSE_MEMORY_ERROR;
+
     new_course->preCourses = pre_courses;
     new_course->id = duplicateString(id);
     new_course->name = duplicateString(name);
     new_course->credits = credits;
     *course = new_course;
+
     return COURSE_OK;
 }
 
-int coursesEqualId(Course course1, Course course2)
-{
+int coursesEqualId(Course course1, Course course2) {
     assert(course1 != NULL && course2 != NULL);
     int compare_result;
     compare_result = compareCoursesById(course1, course2);
-    if (compare_result == 0)
-    {
+    if (compare_result == 0) {
         return 1;
-    }
-    else
-    {
+    } else {
         return 0;
     }
 }
 
-int courseLessThan(Course course1, Course course2)
-{
+//checks if course1 is less than course2
+int courseLessThan(Course course1, Course course2) {
     assert(course1 != NULL && course2 != NULL);
     int compare_result;
-    compare_result = compareCoursesById(course1,course2);
-    if (compare_result < 0)
-    {
+    compare_result = compareCoursesById(course1, course2);
+    if (compare_result < 0) {
         return 1;
-    }
-    else
-    {
+    } else {
         return 0;
     }
 }
@@ -75,9 +90,50 @@ CourseResult courseUpdateName(Course course1, char *new_name) {
     assert(course1 != NULL && new_name != NULL);
     char *temp_store_name = course1->name;
     char *name = duplicateString(new_name);
-    if (name == NULL)   return COURSE_MEMORY_ERROR;
+    if (name == NULL) return COURSE_MEMORY_ERROR;
+
     course1->name = name;
     free(temp_store_name);
+
+    return COURSE_OK;
+}
+
+CourseResult addPreCourse(Course course1, Course course2) {
+    assert(course1 != NULL && course2 != NULL);
+    if (coursesEqualId(course1, course2) == 1) return COURSE_THE_SAME_COURSE;
+    if (isPreCourseExist(course1, course2) == 1) return COURSE_ALREADY_EXISTS;
+
+    int pre_course_size = size(course1->preCourses);
+    if (pre_course_size == 0) {
+        CHECK_DA_MEMORY_ERROR(addElementStart(course1->preCourses, course2));
+        return COURSE_OK;
+    }
+    else {
+        Course pre_course;
+        for (int i = 0; i < pre_course_size; i++) {
+            pre_course = getPreCourse(course1, i);
+            if (courseLessThan(course2, pre_course) == 1) {
+                CHECK_DA_MEMORY_ERROR(
+                        addElementBefore(course1->preCourses, course2,
+                                         i));
+                return COURSE_OK;
+            }
+        }
+        CHECK_DA_MEMORY_ERROR(addElementEnd(course1->preCourses, course2));
+        return COURSE_OK;
+    }
+}
+
+CourseResult removePreCourse(Course course1, Course course2) {
+    assert(course1 != NULL && course2 != NULL);
+    if (isPreCourseExist(course1, course2) == 0) return COURSE_NOT_EXIST;
+
+    DAResult temp;
+    int index_of_course2;
+    temp = indexOfElement(course1->preCourses, course2, 0, &index_of_course2);
+    if(temp == DA_ILLEGAL_INDEX) return COURSE_ILLEGAL_PARAMETER;
+
+    CHECK_DA_MEMORY_ERROR(removeElement(course1->preCourses,index_of_course2));
     return COURSE_OK;
 }
 
@@ -90,13 +146,13 @@ void displayCourse(Course course1) {
         for (int i = 0; i < size(course1->preCourses); ++i) {
             displayElement(course1->preCourses, i);
         }
+        to_pre_course = 0;
     } else {
         printf("%s ", course1->id);
     }
 }
 
-void destroyCourse(Course course1)
-{
+void destroyCourse(Course course1) {
     free(course1->id);
     free(course1->name);
     destroyDynamicArray(course1->preCourses);
--- CourseSystem.c
+++ CourseSystem.c
@@ -21,17 +21,15 @@ static char *duplicateString(const char *str) {
     return duplicated_string;
 }
 
-CourseSystem createSystem(char *name) {
-    CourseSystem system = malloc(sizeof(CourseSystem));
-    if (system == NULL) return NULL;
-    DynamicArray courses = createDynamicArray();
-    if (courses == NULL) return NULL;
+static Course findCourseById(CourseSystem system, char *course_id) {
+    for (int i = 0; i < size(system->courses); ++i) {
+        Course currect_course = getCourse(system, i);
+        if (strcmp(currect_course->id, course_id) == 0) {
+            return currect_course;
+        }
+    }
 
-    char *duplicated_name = duplicateString(name);
-    system->name = duplicated_name;
-    system->courses = courses;
-
-    return system;
+    return NULL;
 }
 
 static int isCourseExist(CourseSystem system, Course course) {
@@ -48,6 +46,48 @@ static int isCourseExist(CourseSystem system, Course course) {
 static Course getCourse(CourseSystem system, int index) {
     return system->courses->elements[index];
 }
+
+static DynamicArray getPreCourses(Course course) {
+    return course->preCourses;
+}
+
+int sysNumCourses(CourseSystem sys) {
+    assert(sys != NULL);
+    return size(sys->courses);
+}
+
+SysResult sysRemoveCourse(CourseSystem sys, char *course_id) {
+    assert(sys != NULL && course_id != NULL);
+    Course course_to_be_removed = findCourseById(sys, course_id);
+    if (course_to_be_removed == NULL) return SYS_NOT_IN_SYSTEM;
+
+    Course course_in_system;
+    for (int i = 0; i < size(sys->courses); i++) {
+        //TODO: should check DA error and not course as stated in the word file
+        course_in_system = getCourse(sys->courses, i);
+        CHECK_DA_MEMORY_ERROR(removePreCourse(course_in_system->preCourses,
+                                              course_to_be_removed));
+    }
+    CHECK_DA_MEMORY_ERROR(removeElement(sys->courses, course_to_be_removed));
+    destroyCourse(course_to_be_removed);
+
+    return SYS_OK;
+
+}
+
+CourseSystem createSystem(char *name) {
+    CourseSystem system = malloc(sizeof(CourseSystem));
+    if (system == NULL) return NULL;
+    DynamicArray courses = createDynamicArray();
+    if (courses == NULL) return NULL;
+
+    char *duplicated_name = duplicateString(name);
+    system->name = duplicated_name;
+    system->courses = courses;
+
+    return system;
+}
+
 
 SysResult sysAddCourse(CourseSystem system, Course course) {
     assert(system != NULL && course != NULL);
@@ -71,20 +111,6 @@ SysResult sysAddCourse(CourseSystem system, Course course) {
     return SYS_OK;
 }
 
-static Course findCourseById(CourseSystem system, char *course_id) {
-    for (int i = 0; i < size(system->courses); ++i) {
-        Course currect_course = getCourse(system, i);
-        if (strcmp(currect_course->id, course_id) == 0) {
-            return currect_course;
-        }
-    }
-
-    return NULL;
-}
-
-static DynamicArray getPreCourses(Course course) {
-    return course->preCourses;
-}
 
 SysResult sysIsPreCourse(CourseSystem system, char *course_id1, char
 *course_id2, int *result) {
@@ -102,6 +128,20 @@ SysResult sysIsPreCourse(CourseSystem system, char *course_id1, char
     int index;
     indexOfElement(getPreCourses(course1), course2, 0, &index);
     if (index != -1) *result = 1;
+
+    return SYS_OK;
+}
+
+SysResult
+sysAddPreCourse(CourseSystem sys, char *course_id1, char *course_id2) {
+    assert(sys != NULL && course_id1 != NULL && course_id2 != NULL);
+    Course course1, course2;
+    course1 = findCourseById(sys, course_id1);
+    if (course1 == NULL) return SYS_NOT_IN_SYSTEM;
+    course2 = findCourseById(sys, course_id2);
+    if (course2 == NULL) return SYS_NOT_IN_SYSTEM;
+
+    CHECK_DA_MEMORY_ERROR(addPreCourse(course1, course2));
 
     return SYS_OK;
 }
@@ -125,9 +165,36 @@ SysResult sysRemovePreCourse(CourseSystem system, char *course_id1, char
     return SYS_OK;
 }
 
+SysResult
+sysUpdateCourseName(CourseSystem sys, char *course_id, char *new_name) {
+    assert(sys != NULL && course_id != NULL && new_name != NULL);
+    char *duplicated_name = duplicateString(new_name);
+    char *old_name;
+    if (duplicated_name == NULL) return SYS_MEMORY_PROBLEM;
+
+    Course new_course = findCourseById(sys, course_id);
+    old_name = new_course->name;
+    if (new_course == NULL) return SYS_NOT_IN_SYSTEM;
+    courseUpdateName(new_course, duplicated_name);
+    free(old_name);
+
+    return SYS_OK;
+}
+
 void displaySystem(CourseSystem system) {
     assert(system != NULL);
 
     printf("%s\n", system->name);
     displayAllElements(system->courses);
+}
+
+
+void destroySystem(CourseSystem sys) {
+    for (int i = 0; i < size(sys->courses); i++) {
+        sysRemoveCourse(sys->courses, i);
+    }
+    free(sys->name);
+    CHECK_DA_MEMORY_ERROR(destroyDynamicArray(sys->courses));
+    free(sys);
+    return SYS_OK;
 }
